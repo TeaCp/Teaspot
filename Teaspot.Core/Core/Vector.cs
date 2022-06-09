16da18cef8738b95cbeb3547f05952b493339ec0
almakael <almakael@list.ru>
2022-06-09 22:18:46 +0300

    added some operators and methods to vector class

--- Teaspot.Core/Core/Vector.cs
+++ Teaspot.Core/Core/Vector.cs
@@ -8,19 +8,35 @@ namespace Teaspot.Core
 {
     public struct Vector
     {
-        public float X { get; set; }
-        public float Y { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
-        public Vector(float x, float y)
+        public Vector(int x, int y)
         {
-            X = x;
-            Y = y;
+            this.X = x;
+            this.Y = y;
         }
 
-        public static Vector Right { get { return new Vector(1f, 0f); } }
-        public static Vector Top { get { return new Vector(0f, 1f); } }
-        public static Vector Left { get { return new Vector(-1f, 0f); } }
-        public static Vector Bottom { get { return new Vector(0f, -1f); } }
+        public static Vector Right { get { return new Vector(1, 0); } }
+        public static Vector Top { get { return new Vector(0, 1); } }
+        public static Vector Left { get { return new Vector(-1, 0); } }
+        public static Vector Bottom { get { return new Vector(0, -1); } }
 
+        public static Vector operator +(Vector vector) => vector;
+        public static Vector operator -(Vector vector) => new Vector(-vector.X, -vector.Y);
+        public static Vector operator +(Vector left, Vector right)
+        => new Vector(left.X + right.X, left.Y + right.Y);
+        public static Vector operator -(Vector left, Vector right)
+        => left + (-right);
+        public static Vector operator *(int multiplier, Vector vector)
+        => new Vector(multiplier * vector.X, multiplier * vector.Y);
+        public static Vector operator *(Vector vector, int multiplier)
+        => multiplier * vector;
+
+        public int getLength () { return Convert.ToInt32(Math.Sqrt(this.X*this.X + this.Y*this.Y)); }
+        public static int getAngleBetween (Vector left, Vector right)
+        {
+            return Math.Abs(left.X*right.X + left.Y*right.Y)/(left.getLength() * right.getLength());
+        }
     }
 }
