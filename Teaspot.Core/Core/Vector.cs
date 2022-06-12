c8dedf6876467659198637c2791063b3f123d5a7
dkrsk <wileps2310@yandex.ru>
2022-06-12 18:01:11 +0300

    vector cleans :)

--- Teaspot.Core/Core/Vector.cs
+++ Teaspot.Core/Core/Vector.cs
@@ -8,6 +8,7 @@
         public Vector (Point endOfRadiusVector) : this(endOfRadiusVector.X, endOfRadiusVector.Y) { }
         public Vector (Point begin, Point end) : this(end - begin) { }
 
+        public static Vector Zero => new Vector(0, 0);
         public static Vector UnitRight => new Vector(1f, 0f);
         public static Vector UnitTop => new Vector(0f, 1f);
         public static Vector UnitLeft => -UnitRight;
@@ -26,12 +27,6 @@
         public static double getScalarProduction (Vector left, Vector right) => left.Pos.X*right.Pos.X + left.Pos.Y*right.Pos.Y;
         public static double getAngleBetween (Vector left, Vector right) =>
             Math.Acos(getScalarProduction(left, right) / (left.getLength() * right.getLength()));
-
-        public static Vector Zero => new Vector(0, 0);
-        public static Vector Right => new Vector(1, 0);
-        public static Vector Left => new Vector(-1, 0);
-        public static Vector Top => new Vector(0, 1);
-        public static Vector Bottom => new Vector(0, -1);
 
     }
 }
